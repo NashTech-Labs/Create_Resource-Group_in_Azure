3e2d642152900badecf04bf59bd81b465ab6a543
Vikas Vashisth <76785155+VikasVashisth@users.noreply.github.com>
2023-02-15 12:19:39 +0530

    Update create-resource-group.sh

--- create-resource-group.sh
+++ create-resource-group.sh
@@ -9,21 +9,32 @@ read rglocation
 
 az group list -o table | sed 's/\|/ /'|awk '{print $1}' | awk '/^Name|^-----|^Name/ {next}{for (i=1;i<=NF;i++){print $i}}' > rg-list.txt
 az account list-locations --query [].[name] -o table | awk '/^Column1|^-----|^Column1/ {next}{for (i=1;i<=NF;i++){print $i}}' > locations.txt
-
 echo "====Checking ResourceGroup exists or not====="
 
-        if grep -w -i $rgname rg-list.txt && grep -w -i $rglocation locations.txt
+        if grep -w -i $rgname rg-list.txt 
         then
+        az group show --resource-group $rgname -otable | sed 's/\|/ /'|awk '{print $1}' | awk '/^Location|^-----|^Location/ {next}{for (i=1;i<=NF;i++){print $i}}' > match-location.txt
 
-        echo "----ResourceGroup already exist----"
+           if  grep -w -i $rglocation match-location.txt
+            then
 
-        rm rg-list.txt locations.txt
+                echo "----ResourceGroup already exist----"
+               rm rg-list.txt match-location.txt locations.txt
+            else
+
+                echo "----Creating ResourceGroup with the location----"
+
+              az group create --name $rgname --location $rglocation
+
+              rm rg-list.txt match-location.txt locations.txt
+
+            fi
 
         else
-        echo "----Creating ResourceGroup----"
+        echo "----Creating ResourceGroup RG----"
 
-        az group create --name $rgname --location $rglocation
+       az group create --name $rgname --location $rglocation
 
-        rm rg-list.txt locations.txt
+       rm rg-list.txt match-location.txt locations.txt
 
         fi
